d8a33993c09eb3efddf66802b2a22fb86133820e
Bulat <bulatsharipov240@gmail.com>
2020-01-13 17:11:37 +0300

    made some auth and parser

---
+++ parser.py
@@ -0,0 +1,57 @@
+import pytools
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+import authorization
+
+LOGIN = authorization.load_data()[0]
+PASSWORD = authorization.load_data()[1]
+URL_LOG = 'https://edu.tatar.ru/logon/'
+DIARY_URL = "https://edu.tatar.ru/user/diary/term"
+
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "ru,en;q=0.9",
+    "Cache-Control": "max-age=0",
+    "Connection": "keep-alive",
+    "Content-Length": "40",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Host": "edu.tatar.ru",
+    "Origin": "https://edu.tatar.ru",
+    "Referer": "https://edu.tatar.ru/logon/",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 '
+                  'YaBrowser/19.12.3.320 Yowser/2.5 Safari/537.36 ',
+}
+tbd = ["<td>", "</td>", "просмотр", ".", "\n", "", " просмотр\n", "-"]
+
+data = {
+    "main_login": LOGIN,
+    "main_password": PASSWORD,
+}
+
+cookies = pytools.load("cookies.data")
+
+session = requests.Session()
+session.cookies = cookies
+response = session.get(DIARY_URL)
+global soup
+soup = bs(response.content, "html.parser")
+
+dick = dict()
+
+def parse():
+    table = soup.find('table')
+
+    rows = table.findAll('tr')[:-1]
+
+    for row in rows[1:]: dick[row.find('td').text] = [word.text for word in row.findAll('td')[1:]]
+
+    for i in dick: dick[i] = [word for word in dick[i] if word not in tbd]
+
+    for i in dick:
+        for j in dick[i]:
+            if "." in j:
+                dick[i].remove(j)
