3f1b0079a2a454788b1d96ed0f96604177302a0e
Bulat <bulatsharipov240@gmail.com>
2020-01-15 19:11:09 +0300

    some minor changes

--- bot.py
+++ bot.py
@@ -19,100 +19,126 @@ vk = vk_api.VkApi(token=TOKEN)
 vk_session = vk.get_api()
 longpoll = VkLongPoll(vk)
 
-
-def write_msg(user_id, message, rand=None):
-    random_id = random.randint(10, 100000) if rand == None else rand
-    vk_session.messages.send(user_id=user_id, message=message, random_id=random_id)
-    print("Ответил", message)
-
-
-def get_last_bot_message(user_id):
-    for message in vk_session.messages.getHistory(user_id=user_id)['items']:
-        if message['from_id'] == MY_ID:
-            return {"text": message['text'], "random_id": message["random_id"]}
+common_dick = {
+    "ПРИВЕТ" : "ПРИВЕТ",
+    "САФЭН":"ЭРИК",
+    "ГРАДУСЫ":"ЛУЧШАЯ ГРУППА",
+    "СКИНЬ ЖОПУ":"АХ ЕСЛИ БЫ Я МОГ...",
+    "ДАША":"НЕ НАПИСАЛА",
+    "КАИ":"ПУП ЗЕМЛИ",
+}
 
 
-def get_last_user_message(user_id):
-    for message in vk_session.messages.getHistory(user_id=user_id)['items']:
-        if message['from_id'] != MY_ID:
-            return {"text": message['text'], "random_id": message["random_id"]}
+class Bot:
+    def __init__(self):
+        self.auth = False
+        self.general_message = f"Мой функционал предельно прост. Я отправляю оценки и считаю баллы " \
+            f"Если ты еще не авторизовался,напиши 'авторизация', если да, пиши 'оценки'. На этом собственно все, " \
+            f"удачного использования "
 
+    @staticmethod
+    def write_msg(user_id, message, rand=None):
+        random_id = random.randint(10, 100000) if rand == None else rand
+        vk_session.messages.send(user_id=user_id, message=message, random_id=random_id)
+        print("Ответил", message)
 
-def register_new_user(event):
-    last_bot_message = get_last_bot_message(event.user_id)
-    global login, password, cookies
-    if last_bot_message['text'] == MSG_CODES['login']:
-        login = event.text
-        try:
-            login = int(login)
-            write_msg(event.user_id, "Здорово, теперь нам нужен ваш пароль)")
-        except ValueError:
-            write_msg(event.user_id, "Что-то не так с логином")
-            write_msg(event.user_id, MSG_CODES['login'])
-    elif last_bot_message['text'] == MSG_CODES['password']:
-        password = event.text.upper()
-        print("got the password", password)
-        print("got the login", login)
-        user_info = edu_handler.edu_auth(login, password)
-        if user_info['name'] != "не удалость авторизоваться":
-            name = user_info['name']
-            cookies = user_info['cookies']
-            write_msg(event.user_id, name)
-            write_msg(event.user_id, MSG_CODES['confirm'])
+    @staticmethod
+    def get_last_bot_message(user_id):
+        for message in vk_session.messages.getHistory(user_id=user_id)['items']:
+            if message['from_id'] == MY_ID:
+                return {"text": message['text'], "random_id": message["random_id"]}
+
+    @staticmethod
+    def get_last_user_message(user_id):
+        for message in vk_session.messages.getHistory(user_id=user_id)['items']:
+            if message['from_id'] != MY_ID:
+                return {"text": message['text'], "random_id": message["random_id"]}
+
+    def register_new_user(self, event):
+        last_bot_message = self.get_last_bot_message(event.user_id)
+        global login, password, cookies
+        if last_bot_message['text'] == MSG_CODES['login']:
+            login = event.text
+            try:
+                login = int(login)
+                self.write_msg(event.user_id, "Здорово, теперь нам нужен ваш пароль)")
+            except ValueError:
+                self.write_msg(event.user_id, "Что-то не так с логином")
+                self.write_msg(event.user_id, MSG_CODES['login'])
+        elif last_bot_message['text'] == MSG_CODES['password']:
+            password = event.text.upper()
+            print("got the password", password)
+            print("got the login", login)
+            user_info = edu_handler.edu_auth(login, password)
+            if user_info['name'] != "не удалость авторизоваться":
+                name = user_info['name']
+                cookies = user_info['cookies']
+                self.write_msg(event.user_id, name)
+                self.write_msg(event.user_id, MSG_CODES['confirm'])
+            else:
+                self.write_msg(event.user_id, user_info['name'])
+                self.write_msg(event.user_id, MSG_CODES['login'])
+
+        elif last_bot_message['text'] == MSG_CODES['confirm']:
+            if event.text.upper() == "ДА":
+                authorization.save_user_data(login, password, event.user_id, cookies)
+                self.write_msg(event.user_id, "Все данные успешно сохранены")
+                self.auth = False
+            elif event.text.upper() == "НЕТ":
+                self.write_msg(event.user_id, "В следующий раз все получится)")
+                self.write_msg(event.user_id, MSG_CODES['login'])
+            else:
+                self.write_msg(event.user_id, "Так сложно было написать да/нет? Теперь заново придется")
+                self.write_msg(event.user_id, MSG_CODES['login'])
+
         else:
-            write_msg(event.user_id, user_info['name'])
-            write_msg(event.user_id, MSG_CODES['login'])
+            self.write_msg(event.user_id, MSG_CODES['login'])
 
-    elif last_bot_message['text'] == MSG_CODES['confirm']:
-        if event.text.upper() == "ДА":
-            authorization.save_user_data(login, password, event.user_id, cookies)
-            write_msg(event.user_id, "Все данные успешно сохранены")
-            auth = False
-        elif event.text.upper() == "НЕТ":
-            write_msg(event.user_id, "В следующий раз все получится)")
-            write_msg(event.user_id, MSG_CODES['login'])
+    def give_marks(self, event):
+        base = authorization.load_data()
+
+        if str(event.user_id) in base.keys():
+            self.write_msg(event.user_id, "О, ты уже авторизован!")
+            data = base[str(event.user_id)]
+            subjects = edu_handler.parse(data[2])
+            message = ""
+            for subject in subjects.keys():
+                message += f"{subject} : {''.join(subjects[subject] or ' ')} \n"
+                marks = [int(mark) for mark in subjects[subject]]
+                message += calc.main(marks) + "\n"
+
+                self.write_msg(event.user_id, message)
         else:
-            write_msg(event.user_id, "Так сложно было написать да/нет? Теперь заново придется")
-            write_msg(event.user_id, MSG_CODES['login'])
+            self.write_msg(event.user_id, "Судя по всему ты еще не авторизовался")
+            self.register_new_user(event)
 
-    else:
-        write_msg(event.user_id, MSG_CODES['login'])
+    def run(self):
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me and not event.from_me:
+                print(f"Получено новое сообщение от {vk_session.users.get(user_ids=event.user_id)[0]['first_name']}")
+                print("Текст", event.text)
 
+                if event.text.upper() == "АВТОРИЗАЦИЯ":
+                    self.auth = True
+                    self.write_msg(event.user_id, MSG_CODES['login'])
+                    continue
+                elif event.text.upper() == "ОЦЕНКИ":
+                    if event.user_id in authorization.load_data().keys():
+                        self.give_marks(event)
+                    else:
+                        self.write_msg(event.user_id,"Ты ещё не авторизовался")
+                        self.auth = True
+                        self.write_msg(event.user_id,MSG_CODES['login'])
 
-def give_marks(event):
-    base = authorization.load_data()
+            elif event.text.upper in common_dick.keys():
+                self.write_msg(event.text,common_dick[event.text.upper()].capitalize())
 
-    if str(event.user_id) in base.keys():
-        write_msg(event.user_id, "О, ты уже авторизован!")
-        data = base[str(event.user_id)]
-        subjects = edu_handler.parse(data[2])
-        message = ""
-        for subject in subjects.keys():
-            message += f"{subject} : {''.join(subjects[subject] or ' ')} \n"
-            marks = [int(mark) for mark in subjects[subject]]
-            message += calc.main(marks) + "\n"
+            else:
+                self.write_msg(event.user_id,self.general_message)
 
-        write_msg(event.user_id, message)
-    else:
-        write_msg(event.user_id,"Судя по всему ты еще не авторизовался")
-        register_new_user(event)
-
-
-def run():
-    global auth
-    auth = False
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me and not event.from_me:
-            print(f"Получено новое сообщение от {vk_session.users.get(user_ids=event.user_id)[0]['first_name']}")
-            print("Текст", event.text)
-
-            if event.text.upper() == "АВТОРИЗАЦИЯ" and event.user_id:
-                auth = True
-                write_msg(event.user_id, "Итак, введите свой логин еду")
-                continue
-
-            if auth:
-                register_new_user(event)
+                if self.auth:
+                    self.register_new_user(event)
 
 
-
+bot = Bot()
+bot.run()
